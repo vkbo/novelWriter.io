e03d70ac56afc7c10226212280ffe64ec5ebb7fd
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2023-02-22 19:55:43 +0100

    Add basic setup of new site

---
+++ source/conf.py
@@ -0,0 +1,79 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from datetime import datetime
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "novelWriter"
+copyright = f"2018–{datetime.now().year} Veronica Berglyd Olsen"
+author = "Veronica Berglyd Olsen"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx_design"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
+html_theme_options = {
+    "navbar_align": "left",
+    "navbar_start": ["navbar-logo"],
+    "navbar_center": ["navbar-nav"],
+    "navbar_end": ["navbar-icon-links"],
+    "navbar_persistent": ["search-button", "theme-switcher"],
+    "logo": {
+        "image_light": "images/novelwriter-light.png",
+        "image_dark": "images/novelwriter-dark.png",
+        # "image_light": "images/novelwriter-icon.svg",
+        # "image_dark": "images/novelwriter-icon.svg",
+    },
+    "favicons": [
+        {
+            "rel": "icon",
+            "sizes": "16x16",
+            "href": "images/novelwriter-icon-16.png",
+        },
+        {
+            "rel": "icon",
+            "sizes": "32x32",
+            "href": "images/novelwriter-icon-32.png",
+        },
+        {
+            "rel": "apple-touch-icon",
+            "sizes": "180x180",
+            "href": "images/novelwriter-icon-180.png",
+        },
+    ],
+    "pygment_light_style": "tango",
+    "pygment_dark_style": "monokai",
+    "icon_links_label": "Quick Links",
+    "icon_links": [
+        {
+            "name": "Mastodon",
+            "url": "https://fosstodon.org/@novelwriter",
+            "icon": "fa-brands fa-mastodon",
+        },
+        {
+            "name": "GitHub",
+            "url": "https://github.com/vkbo/novelwriter",
+            "icon": "fa-brands fa-github",
+        },
+        {
+            "name": "PyPi",
+            "url": "https://pypi.org/project/novelWriter",
+            "icon": "fa-solid fa-box",
+        },
+    ],
+}
---
+++ source/index.rst
@@ -0,0 +1,43 @@
+:html_theme.sidebar_secondary.remove:
+
+novelWriter
+===========
+
+**A markdown-like editor for novels**
+
+.. grid:: 2
+   :margin: 4 4 0 0
+   :gutter: 0
+   :padding: 0
+
+   .. grid-item::
+      :padding: 0 0 0 3
+
+      A markdown-like text editor designed for writing novels and larger projects of many smaller
+      plain text documents.
+
+      It is designed to be a simple text editor that allows for easy organisation of text files and
+      notes, with a meta data syntax for comments, synopsis, and cross-referencing between files,
+      and built on plain text files for robustness.
+
+   .. grid-item::
+      :padding: 0 0 3 0
+
+      .. image:: _static/images/screenshot-multi.png
+         :width: 100%
+
+.. code-block:: python
+
+   import blablabla
+
+   for a in range(14):
+       print(a**2)
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents
+   :hidden:
+
+   Home <self>
+   Manual <https://novelwriter.readthedocs.io/>
+
